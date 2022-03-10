8e572811ec20402382be43c9bab148c63e9dd76d
Michael Edgar <michael@xlate.io>
2022-03-10 17:54:03 -0500

    Demo issue https://github.com/quarkusio/quarkus/issues/23592

--- getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
+++ getting-started/src/main/java/org/acme/getting/started/GreetingResource.java
@@ -1,12 +1,17 @@
 package org.acme.getting.started;
 
 import javax.inject.Inject;
+import javax.ws.rs.BeanParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-import org.jboss.resteasy.annotations.jaxrs.PathParam;
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 
 @Path("/hello")
 public class GreetingResource {
@@ -15,15 +20,85 @@ public class GreetingResource {
     GreetingService service;
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    @Path("/greeting/{name}")
-    public String greeting(@PathParam String name) {
-        return service.greeting(name);
+    @Path("/world")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CompletionStage<Response> helloWorld(@BeanParam OrderByInput<Key1> orderParams) {
+        return CompletableFuture.completedStage(Response.ok("World").build());
     }
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+    @Path("/there")
+    @Produces(MediaType.APPLICATION_JSON)
+    public CompletionStage<Response> helloThere(@BeanParam OrderByInput<Key2> orderParams) {
+        return CompletableFuture.completedStage(Response.ok("There").build());
+    }
+
+    public static class OrderByInput<T> {
+        @QueryParam("orderKey")
+        T field;
+
+        @QueryParam("order")
+        String order;
+
+        public T getField() {
+            return field;
+        }
+
+        public void setField(T field) {
+            this.field = field;
+        }
+
+        public String getOrder() {
+            return order;
+        }
+
+        public void setOrder(String order) {
+            this.order = order;
+        }
+
+    }
+
+    public enum Key1 {
+        NAME("name"),
+        DATE("date");
+
+        String value;
+
+        private Key1(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public static Key1 fromString(String value) {
+            return Arrays.stream(values())
+                    .filter(v -> v.getValue().equals(value))
+                    .findFirst()
+                    .orElse(null);
+        }
+    }
+
+    public enum Key2 {
+        NAME("name"),
+        DATE("date");
+
+        String value;
+
+        private Key2(String value) {
+            this.value = value;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public static Key2 fromString(String value) {
+            return Arrays.stream(values())
+                    .filter(v -> v.getValue().equals(value))
+                    .findFirst()
+                    .orElse(null);
+        }
     }
 }
